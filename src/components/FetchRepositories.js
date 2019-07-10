46073f042265165ca42bc65cdfad59f0be15a116
Yotam Bloom <yotam.bloom@wework.com>
2019-07-10 16:11:54 +0300

    final

---
+++ src/components/FetchRepositories.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+export default class FetchRepositories extends React.Component {
+  state = {
+    data: null,
+    loading: false,
+    error: null,
+  }
+
+  componentDidMount() {
+    this.fetch()
+  }
+
+  componentDidUpdate(prev) {
+    if (prev.username !== this.props.username) this.fetch()
+  }
+
+  fetch = () => {
+    this.setState({
+      loading: true,
+    })
+    fetch(
+      `https://api.github.com/users/${this.props.username}/repos?sort=pushed&access_token={PRIVATE_ACCESS_TOKEN}`,
+    )
+      .then(res => res.json())
+      .then(repositories => {
+        this.setState({
+          data: repositories,
+          loading: false,
+          error: null,
+        })
+      })
+      .catch(err => {
+        this.setState({
+          data: null,
+          loading: false,
+          error: err.message,
+        })
+      })
+  }
+
+  render() {
+    return this.props.children(this.state)
+  }
+}
